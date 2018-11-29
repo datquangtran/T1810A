3966d1108084e6ba062dbc1e087fd6e31a6511fa
Trong <silentlove9995@gmail.com>
2018-11-29 14:44:42 +0700

    Create baiTapthem1.cpp

---
+++ baiTapthem1.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main (){
+	int x,y;
+	printf("nhap so thu nhat la:",x);
+	scanf ("%d",&x);
+	printf ("nhap so thu hai la :",y);
+	scanf ("%d",&y);
+	if (x>y){
+		printf ("so lon hon la :%d\n",x);
+	}else {
+		printf ("so lon hon la :%d\n",y);
+	}
+	
+	return 0;
+}
