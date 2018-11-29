766e7580895f43d3ad92086b28bcf8f323786538
nguyenthininh <45448845+nguyenthininh@users.noreply.github.com>
2018-11-29 14:46:53 +0700

    Create for 10.cpp
    nop2

---
+++ for 10.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+	int n;
+	do{
+		printf("nhap so n:=\n");
+		scanf("%d",&n);
+	}while(n<1);
+	
+	int ar[n];
+	for(int i=0; i<n; i++){
+		for(int j=1; j>1 && j<100; j++){
+		}
+		printf("nhap phan tu thu %d:\n",i);
+		scanf("%d", &ar[i]);
+	}
+	printf("day so la:\n", j);
+	scanf("%d", ar[j]);
+	
+	return 0;
+}
